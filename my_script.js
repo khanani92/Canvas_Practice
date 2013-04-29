24fcbf540aeabd05f5d735f87cf02403aee3b107
unknown <muddassir_92@hotmail.com>
2013-04-29 12:25:53 -0700

    checking .animation funtion step 1

--- my_script.js
+++ my_script.js
@@ -1,7 +1,12 @@
-/**
- * Created with JetBrains WebStorm.
- * User: aaaa
- * Date: 4/29/13
- * Time: 8:42 AM
- * To change this template use File | Settings | File Templates.
- */
+$(function(){
+    $("#img1").bind("mouseover",function(){
+        $(this).animate({
+          opacity:.5,
+          marginLeft:'10px'
+
+        },
+           1500
+        );
+
+    });//.bind
+});
