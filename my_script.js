44886ebfcc6daf7afce6e6610e35281b60aa2801
unknown <muddassir_92@hotmail.com>
2013-04-29 21:31:05 -0700

    Action after effect is completed, use of Left property(position: relative)

--- my_script.js
+++ my_script.js
@@ -1,14 +1,17 @@
 $(function(){
+    $("#text1").hide();
     $("#img1").bind("mouseover",function(){
         //var marginLeft=  $(this).css("marginLeft");
         //alert(rate);
         $(this).animate({
           opacity:.5,
-          marginLeft:'+=300px'
+          marginLeft:'+=30px',
+                left:'+=100px'
 
         },
            1500,
-            'easeInOutElastic'
+            'easeInOutElastic',
+            function(){$("#text1").show();}
         );
 
     });//.bind
