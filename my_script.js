a0a282bce5317fb619b8575954ea9935527c9758
unknown <muddassir_92@hotmail.com>
2013-04-29 21:11:28 -0700

    Using jquery.easing plugin for animation (http://jqueryui.com/resources/demos/effect/easing.html)

--- my_script.js
+++ my_script.js
@@ -1,11 +1,14 @@
 $(function(){
     $("#img1").bind("mouseover",function(){
+        //var marginLeft=  $(this).css("marginLeft");
+        //alert(rate);
         $(this).animate({
           opacity:.5,
-          marginLeft:'10px'
+          marginLeft:'+=300px'
 
         },
-           1500
+           1500,
+            'easeInOutElastic'
         );
 
     });//.bind
