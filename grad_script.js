b0eb0fc2b3e214d9266e3620307789e7af7530bd
unknown <muddassir_92@hotmail.com>
2013-05-02 07:40:49 -0700

    using linergradient in diffrenet size

--- grad_script.js
+++ grad_script.js
@@ -7,12 +7,25 @@
  */
 $(function(){
     var canvas= document.getElementById("canvas").getContext("2d");
-
+    //simple linear gradient
     var grad = canvas.createLinearGradient(0,0,500,200);
     grad.addColorStop(0,"red");
     grad.addColorStop(0.5,"white");
     grad.addColorStop(1,"black");
 
+    //simple linear gradient size changed
     canvas.fillStyle = grad;
     canvas.fillRect(0,0,500,200);
+    canvas.fillRect(0,300,50,50);
+
+    //simple linear gradient size changed
+    var grad2 = canvas.createLinearGradient(10,10,60,60);
+    grad2.addColorStop(0,"red");
+    grad2.addColorStop(0.5,"white");
+    grad2.addColorStop(1,"black");
+    canvas.fillStyle = grad2;
+    canvas.fillRect(10,10,50,50);
+
+    //simple linear gradient size changed
+
 });
